fbd4ad22f0ee644980931dc1dd11364e25c5b737
uzairwaheed1 <uzairw227@gmail.com>
2023-02-03 01:16:22 +0500

    header

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 function Header() {
   return (
     <>
-    <div className='header'></div>
+      <div className="header">
+        <div className="logo">
+            <img className="logo-img" src="./The Tech Test (3) (1).png" />
+            <h3 className="logo-text"> The Tech Test </h3>
+            {/* <img className="logo-text" src="./The Tech Test (2) (1).png" /> */}
+
+        </div>
+      </div>
     </>
-    )
+  );
 }
 
-export default Header
+export default Header;
